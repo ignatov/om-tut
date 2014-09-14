a69ed2ae46746d7a588cb1a89f1bb015c658917b
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2014-09-14 21:13:49 +0400

    initial

---
+++ src/om_tut/core.cljs
@@ -0,0 +1,20 @@
+(ns om-tut.core
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]))
+
+(enable-console-print!)
+
+;(def app-state (atom {:text "Hello world!!111111111!!"}))
+
+(def app-state (atom {:list ["Lion" "Zebra" "Buffalo" "Antelope"]}))
+
+(om/root
+  (fn [app owner]
+    (om/component
+      (apply dom/ul nil
+             (map (fn [text] (dom/li nil text)) (:list app)))))
+  app-state
+  {:target (. js/document (getElementById "app"))})
+
+
+;(swap! app-state assoc :text "Do it live!")
